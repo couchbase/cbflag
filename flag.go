8231bae933ec930f486044da01cf83bb89daa3d0
Mike Wiederhold <mikewied06@gmail.com>
2016-08-13 12:23:27 -0700

    Add an parameter to all flags for hidden options
    Hidden options are not shown in the help message.

--- flag.go
+++ flag.go
@@ -30,72 +30,84 @@ type Flag struct {
 	foundLong  bool
 	foundShort bool
 	required   bool
+	hidden     bool
 	isFlag     bool
 }
 
-func BoolFlag(result *bool, def bool, short, long, usage string) *Flag {
-	return varFlag(newBoolValue(def, result), short, long, usage, nil, DefaultOptionHandler, false, true)
+func BoolFlag(result *bool, def bool, short, long, usage string, hidden bool) *Flag {
+	return varFlag(newBoolValue(def, result), short, long, usage, nil, DefaultOptionHandler,
+		false, hidden, true)
 }
 
-func Float64Flag(result *float64, def float64, short, long, usage string, validator ValidatorFn, required bool) *Flag {
-	return varFlag(newFloat64Value(def, result), short, long, usage, validator, DefaultOptionHandler, required, false)
+func Float64Flag(result *float64, def float64, short, long, usage string, validator ValidatorFn, required,
+	hidden bool) *Flag {
+	return varFlag(newFloat64Value(def, result), short, long, usage, validator, DefaultOptionHandler,
+		required, hidden, false)
 }
 
-func IntFlag(result *int, def int, short, long, usage string, validator ValidatorFn, required bool) *Flag {
-	return varFlag(newIntValue(def, result), short, long, usage, validator, DefaultOptionHandler, required, false)
+func IntFlag(result *int, def int, short, long, usage string, validator ValidatorFn, required, hidden bool) *Flag {
+	return varFlag(newIntValue(def, result), short, long, usage, validator, DefaultOptionHandler,
+		required, hidden, false)
 }
 
-func Int64Flag(result *int64, def int64, short, long, usage string, validator ValidatorFn, required bool) *Flag {
-	return varFlag(newInt64Value(def, result), short, long, usage, validator, DefaultOptionHandler, required, false)
+func Int64Flag(result *int64, def int64, short, long, usage string, validator ValidatorFn, required,
+	hidden bool) *Flag {
+	return varFlag(newInt64Value(def, result), short, long, usage, validator, DefaultOptionHandler,
+		required, hidden, false)
 }
 
-func StringFlag(result *string, def, short, long, usage string, validator ValidatorFn, required bool) *Flag {
+func StringFlag(result *string, def, short, long, usage string, validator ValidatorFn, required,
+	hidden bool) *Flag {
 	return varFlag(newStringValue(def, result), short, long, usage, validator, DefaultOptionHandler,
-		required, false)
+		required, hidden, false)
 }
 
-func UintFlag(result *uint, def uint, short, long, usage string, validator ValidatorFn, required bool) *Flag {
+func UintFlag(result *uint, def uint, short, long, usage string, validator ValidatorFn, required,
+	hidden bool) *Flag {
 	return varFlag(newUintValue(def, result), short, long, usage, validator, DefaultOptionHandler,
-		required, false)
+		required, hidden, false)
 }
 
-func Uint64Flag(result *uint64, def uint64, short, long, usage string, validator ValidatorFn, required bool) *Flag {
+func Uint64Flag(result *uint64, def uint64, short, long, usage string, validator ValidatorFn,
+	required, hidden bool) *Flag {
 	return varFlag(newUint64Value(def, result), short, long, usage, validator, DefaultOptionHandler,
-		required, false)
+		required, hidden, false)
 }
 
-func HostFlag(result *string, def string, required bool) *Flag {
+func HostFlag(result *string, def string, required, hidden bool) *Flag {
 	return varFlag(newStringValue(def, result), "c", "cluster", "The hostname of the Couchbase cluster",
-		HostValidator, DefaultOptionHandler, required, false)
+		HostValidator, DefaultOptionHandler, required, hidden, false)
 }
 
-func UsernameFlag(result *string, def string, required bool) *Flag {
+func UsernameFlag(result *string, def string, required, hidden bool) *Flag {
 	return varFlag(newStringValue(def, result), "u", "username", "The hostname of the Couchbase cluster",
-		nil, DefaultOptionHandler, required, false)
+		nil, DefaultOptionHandler, required, hidden, false)
 }
 
-func PasswordFlag(result *string, def string, required bool) *Flag {
+func PasswordFlag(result *string, def string, required, hidden bool) *Flag {
 	return varFlag(newStringValue(def, result), "p", "password", "The password of the Couchbase cluster",
-		nil, PasswordOptionHandler, required, false)
+		nil, PasswordOptionHandler, required, hidden, false)
 }
 
-func CACertFlag(result *string, def string, required bool) *Flag {
+func CACertFlag(result *string, def string, required, hidden bool) *Flag {
 	return varFlag(newStringValue(def, result), "", "cacert",
-		"Verifies the cluster identity with this certificate", nil, DefaultOptionHandler, required, false)
+		"Verifies the cluster identity with this certificate", nil, DefaultOptionHandler,
+		required, hidden, false)
 }
 
-func NoSSLVerifyFlag(result *bool, required bool) *Flag {
+func NoSSLVerifyFlag(result *bool, required, hidden bool) *Flag {
 	return varFlag(newBoolValue(false, result), "", "no-ssl-verify",
-		"Skips SSL verification of certificates against CA", nil, DefaultOptionHandler, required, true)
+		"Skips SSL verification of certificates against CA", nil, DefaultOptionHandler,
+		required, hidden, true)
 }
 
 func helpFlag(result *bool) *Flag {
 	return varFlag(newBoolValue(false, result), "h", "help", "Prints the help message", nil,
-		DefaultOptionHandler, false, true)
+		DefaultOptionHandler, false, false, true)
 }
 
 func varFlag(value Value, short, long, usage string, validator ValidatorFn, optHandler OptionHandler,
-	required, isFlag bool) *Flag {
+	required, hidden, isFlag bool) *Flag {
 	return &Flag{
 		short:      short,
 		long:       long,
@@ -106,6 +118,7 @@ func varFlag(value Value, short, long, usage string, validator ValidatorFn, optH
 		foundLong:  false,
 		foundShort: false,
 		required:   required,
+		hidden:     hidden,
 		isFlag:     isFlag,
 	}
 }
@@ -131,6 +144,10 @@ func (f *Flag) validate() error {
 }
 
 func (f *Flag) usageString() string {
+	if f.hidden {
+		return ""
+	}
+
 	s := ""
 	lines := f.splitDescription()
 
