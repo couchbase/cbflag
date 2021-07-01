54f929f0d126bf134601ace36ffbc4197c00ffbe
Blair Watt <blair.watt@couchbase.com>
2021-07-01 11:34:42 +0100

    CBD-4161: Convert remaining Server repositories to BSL
    Change-Id: I7dafc5ebe229dca8edf534a8242029f8da72645e
    Reviewed-on: http://review.couchbase.org/c/cbflag/+/156757
    Reviewed-by: Patrick Varley <patrick@couchbase.com>
    Reviewed-by: Simon Murray <simon.murray@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- man/man_windows.go
+++ man/man_windows.go
@@ -1,8 +1,14 @@
 // +build windows
 
-/* Copyright (C) Couchbase, Inc 2016 - All Rights Reserved
+/* Copyright 2016-Present Couchbase, Inc.
+ *
  * Unauthorized copying of this file, via any medium is strictly prohibited
- * Proprietary and confidential
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ * in that file, in accordance with the Business Source License, use of this
+ * software will be governed by the Apache License, Version 2.0, included in
+ * the file licenses/APL2.txt.
  */
 
 package man
